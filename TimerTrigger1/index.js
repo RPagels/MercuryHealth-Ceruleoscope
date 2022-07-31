9169a8c6542e3b87cefae820164cb74228dd12e7
Randy Pagels <rpagels@microsoft.com>
2022-07-31 11:21:25 -0400

    Updates

--- TimerTrigger1/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context, myTimer) {
-    try{
-        const { PlaywrightTestLauncher } = require("ceruleoscope");
-        let responseMessage = await PlaywrightTestLauncher.Run();
-        context.log("Playwright tests console output: " + responseMessage);
-    } catch(ex){
-        context.log("Failed to run Playwright tests: " + ex);
-    }
-};
