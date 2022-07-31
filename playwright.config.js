c22703bab6bd07c2cdcce14a4f183b4c363aa6bb
Randy Pagels <rpagels@microsoft.com>
2022-07-31 15:24:29 -0400

    Updates

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,6 @@
 const config = {
     use: {
-        testDir: 'tests',
+        testDir: 'Tests',
         headless: true,
       viewport: { width: 1280, height: 720 },
       ignoreHTTPSErrors: true,
